04e136bf769a7e770c39f19c3827d66a0ce660aa
Brian <Brian@Brians-MacBook-Pro.local>
2013-03-16 22:04:59 +0000

    Initial save

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ERP::Application.config.secret_token = '5d5006668e9c8f2873b767ac30ba393ce023e39cbc9883901e02d0a4b096d63cd14b5b49cd45869dc6ebf1541cf7931ec8083a6de0bb88021d030aee7442b904'
